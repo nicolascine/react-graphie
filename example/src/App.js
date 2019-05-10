1432bdccd5f0f434e90eee1f8142fa7ddd78a4fe
Nicolás Silva <sovietfrance@gmail.com>
2019-05-09 23:37:56 -0400

    feat(example): add fetch func with new example data

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,24 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
-import ExampleComponent from "react-graphie";
+import Graph from "react-graphie";
 
 export default class App extends Component {
+  state = {
+    data: null
+  };
+  componentDidMount() {
+    fetch(
+      "https://gist.githubusercontent.com/mbostock/4062045/raw/5916d145c8c048a6e3086915a6be464467391c62/miserables.json"
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ data });
+      });
+  }
   render() {
-    return (
-      <div>
-        <ExampleComponent text="Modern React component module" />
-      </div>
-    );
+    const { data } = this.state;
+    return <Fragment>{data && <Graph data={data} />}</Fragment>;
   }
 }
