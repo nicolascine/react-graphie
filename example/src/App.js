6cdc45a0c121dffcb9f5f4df3253122ad6ad57a1
Nicolás Silva <sovietfrance@gmail.com>
2019-05-11 23:55:29 -0400

    feat(example): update fake dataset

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Graph from "react-graphie";
 
-const data = {
+const exampleDataset = {
   nodes: [
     { id: "Myriel", group: 1 },
     { id: "Napoleon", group: 2 },
@@ -10,32 +10,23 @@ const data = {
   ],
   links: [
     { source: "Napoleon", target: "Myriel" },
-    { source: "Mlle.Baptistine", target: "Mlle.Baptistine" },
-    { source: "Mme.Magloire", target: "Mlle.Baptistine" },
+    { source: "Mlle.Baptistine", target: "Myriel" },
+    { source: "Mme.Magloire", target: "Myriel" },
     { source: "Mme.Magloire", target: "Mlle.Baptistine" }
   ]
 };
 
 export default class App extends Component {
   render() {
-    const options = {
-      width: 500,
-      height: 500
-    };
-
     return (
-      <div
-        style={{
-          border: "1px solid gray",
-          width: "500px",
-          height: "500px",
-          display: "block",
-          marginRight: "auto",
-          marginLeft: "auto",
-          marginTop: "150px"
-        }}
-      >
-        <Graph data={data} options={options} />
+      <div className="container">
+        <Graph
+          data={exampleDataset}
+          options={{
+            width: 500,
+            height: 500
+          }}
+        />
       </div>
     );
   }
